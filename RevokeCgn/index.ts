56126e047ecd9fdc7d905b38e86c8f22698a629e
AleDore <alessio85.dore@gmail.com>
2021-01-22 10:19:05 +0100

    [#175908973] Change API method to DELETE

--- RevokeCgn/index.ts
+++ RevokeCgn/index.ts
@@ -36,7 +36,7 @@ const app = express();
 secureExpressApp(app);
 
 // Add express route
-app.post("/api/v1/cgn/:fiscalcode", RevokeCgn(userCgnModel));
+app.delete("/api/v1/cgn/:fiscalcode", RevokeCgn(userCgnModel));
 
 const azureFunctionHandler = createAzureFunctionHandler(app);
 
