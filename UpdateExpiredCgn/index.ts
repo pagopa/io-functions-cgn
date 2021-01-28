0cd3b1d661d716250abf74f76028761ce877144e
AleDore <alessio85.dore@gmail.com>
2021-01-28 14:49:59 +0100

    [#175761736] Remove useless comments

--- UpdateExpiredCgn/index.ts
+++ UpdateExpiredCgn/index.ts
@@ -1,15 +1,4 @@
-﻿/*
- * This function is not intended to be invoked directly. Instead it will be
- * triggered by an orchestrator function.
- *
- * Before running this sample, please:
- * - create a Durable orchestration function
- * - create a Durable HTTP starter function
- * - run 'yarn add durable-functions' from the wwwroot folder of your
- *   function app in Kudu
- */
-
-import { createTableService } from "azure-storage";
+﻿import { createTableService } from "azure-storage";
 import { getConfigOrThrow } from "../utils/config";
 import { getUpdateExpiredCgnHandler } from "./handler";
 
