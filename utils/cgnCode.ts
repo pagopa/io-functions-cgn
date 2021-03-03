814b95038cbe5096c25411c2046baa8b0ae5e78f
Alessio Dore <57567806+AleDore@users.noreply.github.com>
2021-03-03 13:54:55 +0100

    [#175761529] Add Generate OTP API specs (#32)

--- utils/cgnCode.ts
+++ utils/cgnCode.ts
@@ -3,6 +3,7 @@ import { promisify } from "util";
 
 import { isLeft } from "fp-ts/lib/Either";
 import { NonEmptyString } from "italia-ts-commons/lib/strings";
+import { OtpCode } from "../generated/definitions/OtpCode";
 
 // Note that we redeclare the alphabet and the length of the CGN here as a
 // double assurance that the implementation is correct and things will break
@@ -12,8 +13,13 @@ import { NonEmptyString } from "italia-ts-commons/lib/strings";
 export const ALPHABET = "ABCDEFGHILMNOPQRSTUVZ123456789";
 const ALPHABET_LEN = ALPHABET.length;
 
+export const OTP_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const OTP_ALPHABET_LEN = OTP_ALPHABET.length;
+
 // Youth Card codes have a length of 16 characthers
 export const BONUSCODE_LENGTH = 16;
+
+export const OTPCODE_LENGTH = 11;
 
 const asyncRandomBytes = promisify(randomBytes);
 
@@ -33,4 +39,22 @@ export async function genRandomCardCode(
     );
   }
   return cardCode.value;
+}
+
+/**
+ * Generates a new random OTP code
+ */
+export async function generateOtpCode(
+  getAsyncRandomBytes: typeof asyncRandomBytes = asyncRandomBytes
+): Promise<OtpCode> {
+  const randomBuffer = await getAsyncRandomBytes(OTPCODE_LENGTH);
+  const code = [...randomBuffer]
+    .map(b => OTP_ALPHABET[b % OTP_ALPHABET_LEN])
+    .join("");
+  const otpCode = OtpCode.decode(code);
+  if (isLeft(otpCode)) {
+    // this should never happen
+    throw Error(`FATAL: generateOtpCode generated invalid OTP code [${code}]`);
+  }
+  return otpCode.value;
 }
