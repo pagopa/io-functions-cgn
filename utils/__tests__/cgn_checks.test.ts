167175be25b0ea41c9941f19b247f2237929c77c
AleDore <alessio85.dore@gmail.com>
2021-03-19 12:33:51 +0100

    [#177396474] Add Tests

--- utils/__tests__/cgn_checks.test.ts
+++ utils/__tests__/cgn_checks.test.ts
@@ -1,10 +1,15 @@
 import * as date_fns from "date-fns";
 import { isLeft, isRight } from "fp-ts/lib/Either";
 import { FiscalCode } from "italia-ts-commons/lib/strings";
-import { checkCgnRequirements, extractCgnExpirationDate } from "../cgn_checks";
+import {
+  checkCgnRequirements,
+  extractCgnExpirationDate,
+  isEycaEligible
+} from "../cgn_checks";
 
-const anElibibleFiscalCode = "DROLSS85S20H501F" as FiscalCode;
+const anElibibleFiscalCode = "DROLSS95S20H501F" as FiscalCode;
 const anUnElibibleFiscalCode = "DROLSS84S20H501F" as FiscalCode;
+const anEycaElibibleFiscalCode = "DROLSS02S20H501F" as FiscalCode;
 const aWrongFiscalCode = "AAAAAADSB00H000F" as FiscalCode;
 describe("checkCgnRequirements", () => {
   it("should return an Error if there is an error extracting birthDate from FiscalCode", async () => {
@@ -29,6 +34,15 @@ describe("checkCgnRequirements", () => {
       expect(result.value).toEqual(false);
     }
   });
+
+  it("should return true if the given fiscalCode is eligible for overwritten max age bound related to CGN", async () => {
+    const result = await checkCgnRequirements(anUnElibibleFiscalCode, 90).run();
+    const isRightResult = isRight(result);
+    expect(isRightResult).toBeTruthy();
+    if (isRightResult) {
+      expect(result.value).toEqual(true);
+    }
+  });
 });
 describe("extractCgnExpirationDate", () => {
   it("should return an Error if there is an error extracting birthDate from FiscalCode", async () => {
@@ -40,7 +54,38 @@ describe("extractCgnExpirationDate", () => {
     const result = await extractCgnExpirationDate(anElibibleFiscalCode).run();
     expect(isRight(result)).toBeTruthy();
     if (isRight(result)) {
-      expect(date_fns.format(result.value, "yyyy-MM-dd")).toEqual("2021-11-20");
+      expect(date_fns.format(result.value, "yyyy-MM-dd")).toEqual("2031-11-20");
+    }
+  });
+});
+
+describe("isEycaEligible", () => {
+  it("should return an Error if it cannot extract birthDate from FiscalCode", async () => {
+    const result = isEycaEligible(aWrongFiscalCode);
+    expect(isLeft(result)).toBeTruthy();
+  });
+
+  it("should return false if user is not eligible for EYCA", async () => {
+    const result = isEycaEligible(anUnElibibleFiscalCode);
+    expect(isRight(result)).toBeTruthy();
+    if (isRight(result)) {
+      expect(result.value).toEqual(false);
+    }
+  });
+
+  it("should return true if user is eligible for EYCA", async () => {
+    const result = isEycaEligible(anEycaElibibleFiscalCode);
+    expect(isRight(result)).toBeTruthy();
+    if (isRight(result)) {
+      expect(result.value).toEqual(true);
+    }
+  });
+
+  it("should return true if user is a Beta tester", async () => {
+    const result = isEycaEligible(anUnElibibleFiscalCode, 90);
+    expect(isRight(result)).toBeTruthy();
+    if (isRight(result)) {
+      expect(result.value).toEqual(true);
     }
   });
 });
