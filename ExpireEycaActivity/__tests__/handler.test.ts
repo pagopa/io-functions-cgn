ca0d4089c3e8d2c6b638abb996fc22b6413d7f58
AleDore <alessio85.dore@gmail.com>
2022-01-14 18:51:01 +0100

    ExpireEycaActivity refactor

--- ExpireEycaActivity/__tests__/handler.test.ts
+++ ExpireEycaActivity/__tests__/handler.test.ts
@@ -4,7 +4,7 @@ import { FiscalCode } from "@pagopa/ts-commons/lib/strings";
 import { none, some } from "fp-ts/lib/Option";
 import * as TE from "fp-ts/lib/TaskEither";
 import { context } from "../../__mocks__/durable-functions";
-import { cgnActivatedDates } from "../../__mocks__/mock";
+import { cgnActivatedDates, testFail } from "../../__mocks__/mock";
 import { StatusEnum as ActivatedStatusEnum } from "../../generated/definitions/CardActivated";
 import {
   CardPending,
@@ -16,6 +16,8 @@ import { EycaCardActivated } from "../../generated/definitions/EycaCardActivated
 import { CcdbNumber } from "../../generated/eyca-api/CcdbNumber";
 import { UserEycaCard } from "../../models/user_eyca_card";
 import { ActivityInput, getExpireEycaActivityHandler } from "../handler";
+import { pipe } from "fp-ts/lib/function";
+import { toError } from "fp-ts/lib/Either";
 
 const aFiscalCode = "RODFDS82S10H501T" as FiscalCode;
 
@@ -50,53 +52,72 @@ describe("ExpireEycaActivity", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
-  it("should return failure if an error occurs during User Eyca Card retrieve", async () => {
+  it("should throw if an error occurs during User Eyca Card retrieve", async () => {
     findLastVersionByModelIdMock.mockImplementationOnce(() =>
       TE.left(toCosmosErrorResponse(new Error("query error")))
     );
     const expireEycaActivityHandler = getExpireEycaActivityHandler(
       userCgnModelMock as any
     );
-    const response = await expireEycaActivityHandler(context, anActivityInput);
-    expect(response.kind).toBe("FAILURE");
-    if (response.kind === "FAILURE") {
-      expect(response.reason).toBe(
-        "Cannot retrieve User EYCA Card for the provided fiscalCode"
-      );
-    }
+
+    await pipe(
+      TE.tryCatch(
+        () => expireEycaActivityHandler(context, anActivityInput),
+        toError
+      ),
+      TE.bimap(e => {
+        expect(e).toBeDefined();
+        expect(e.message).toContain("TRANSIENT FAILURE");
+      }, testFail)
+    )();
   });
 
-  it("should return failure if no User Eyca Card was found", async () => {
+  it("should return permanent failure if no User Eyca Card was found", async () => {
     findLastVersionByModelIdMock.mockImplementationOnce(() => TE.of(none));
     const expireEycaActivityHandler = getExpireEycaActivityHandler(
       userCgnModelMock as any
     );
-    const response = await expireEycaActivityHandler(context, anActivityInput);
-    expect(response.kind).toBe("FAILURE");
-    if (response.kind === "FAILURE") {
-      expect(response.reason).toBe(
-        "No User EYCA Card found for the provided fiscalCode"
-      );
-    }
+
+    await pipe(
+      TE.tryCatch(
+        () => expireEycaActivityHandler(context, anActivityInput),
+        toError
+      ),
+      TE.bimap(testFail, response => {
+        expect(response.kind).toBe("FAILURE");
+        if (response.kind === "FAILURE") {
+          expect(response.reason).toBe(
+            "PERMANENT FAILURE|ERROR=No User EYCA Card found for the provided fiscalCode"
+          );
+        }
+      })
+    )();
   });
 
-  it("should return failure if User Eyca Card is not Active", async () => {
+  it("should return permanent failure if User Eyca Card is not Active", async () => {
     findLastVersionByModelIdMock.mockImplementationOnce(() =>
       TE.of(some(aUserCardPending))
     );
     const expireEycaActivityHandler = getExpireEycaActivityHandler(
       userCgnModelMock as any
     );
-    const response = await expireEycaActivityHandler(context, anActivityInput);
-    expect(response.kind).toBe("FAILURE");
-    if (response.kind === "FAILURE") {
-      expect(response.reason).toBe(
-        "Cannot expire an EYCA Card that is not ACTIVATED"
-      );
-    }
+    await pipe(
+      TE.tryCatch(
+        () => expireEycaActivityHandler(context, anActivityInput),
+        toError
+      ),
+      TE.bimap(testFail, response => {
+        expect(response.kind).toBe("FAILURE");
+        if (response.kind === "FAILURE") {
+          expect(response.reason).toBe(
+            "PERMANENT FAILURE|ERROR=Cannot expire an EYCA Card that is not ACTIVATED"
+          );
+        }
+      })
+    )();
   });
 
-  it("should return failure if userCgn' s update fails", async () => {
+  it("should throw if userCgn' s update fails", async () => {
     findLastVersionByModelIdMock.mockImplementationOnce(() =>
       TE.of(some(anActivatedUserEycaCard))
     );
@@ -106,11 +127,16 @@ describe("ExpireEycaActivity", () => {
     const expireEycaActivityHandler = getExpireEycaActivityHandler(
       userCgnModelMock as any
     );
-    const response = await expireEycaActivityHandler(context, anActivityInput);
-    expect(response.kind).toBe("FAILURE");
-    if (response.kind === "FAILURE") {
-      expect(response.reason).toBe("Cannot update User EYCA Card");
-    }
+    await pipe(
+      TE.tryCatch(
+        () => expireEycaActivityHandler(context, anActivityInput),
+        toError
+      ),
+      TE.bimap(e => {
+        expect(e).toBeDefined();
+        expect(e.message).toContain("TRANSIENT FAILURE");
+      }, testFail)
+    )();
   });
 
   it("should return success if userCgn' s update success", async () => {
